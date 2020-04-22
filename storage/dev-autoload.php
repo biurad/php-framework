87b95873f249563c1f7c27449f03177d6d5ca9bd
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-04-22 00:28:54 +0000

    modified dev-autolading

--- storage/dev-autoload.php
+++ storage/dev-autoload.php
@@ -11,14 +11,41 @@ declare(strict_types=1);
  * Composer is our best friend. He maintains our dependencies and manage
  * the autoloader very well. Good guy Composer.
  */
-require dirname(__DIR__) . '/vendor/autoload.php';
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Just For Development Mode
-$loader = new Nette\Loaders\RobotLoader();
-$loader->setTempDirectory(__DIR__.'/caches/biurad.robotLoader');
-$loader->setAutoRefresh(true);
-$loader->excludeDirectory(
-    dirname(__DIR__).'/vendor/divineniiquaye/cycle-proxy-factory/tests',
-    dirname(__DIR__).'/vendor/biurad/symfony-security-bridge-old'
-);
-$loader->addDirectory(dirname(__DIR__).'/vendor/biurad', dirname(__DIR__).'/vendor/divineniiquaye')->register();
+$autoloadProject = [
+    'BiuradPHP\\API\\'                      => dirname(__DIR__) . '/vendor/biurad/biurad-api/src',
+    'BiuradPHP\\Cache\\'                    => dirname(__DIR__) . '/vendor/biurad/biurad-caching/src',
+    'BiuradPHP\\Database\\'                 => dirname(__DIR__) . '/vendor/biurad/biurad-database/src',
+    'BiuradPHP\\'                           => dirname(__DIR__) . '/vendor/biurad/biurad-developer-tools/src',
+    'BiuradPHP\\Events\\'                   => dirname(__DIR__) . '/vendor/biurad/biurad-events-bus/src',
+    'BiuradPHP\\MVC\\'                      => dirname(__DIR__) . '/vendor/biurad/biurad-framework/src',
+    'BiuradPHP\\Support\\'                  => dirname(__DIR__) . '/vendor/biurad/biurad-helpers/src',
+    'BiuradPHP\\'                           => dirname(__DIR__) . '/vendor/biurad/biurad-i18n/src',
+    'BiuradPHP\\Loader\\'                   => dirname(__DIR__) . '/vendor/biurad/biurad-loader/src',
+    'BiuradPHP\\Security\\'                 => dirname(__DIR__) . '/vendor/biurad/biurad-security/src',
+    'BiuradPHP\\Session\\'                  => dirname(__DIR__) . '/vendor/biurad/biurad-sessions/src',
+    'BiuradPHP\\Template\\'                 => dirname(__DIR__) . '/vendor/biurad/biurad-templating/src',
+    'BiuradPHP\\'                           => dirname(__DIR__) . '/vendor/biurad/cycle-orm-bridge/src',
+    'BiuradPHP\\Routing\\'                  => dirname(__DIR__) . '/vendor/biurad/flight-routing-bridge/src',
+    'BiuradPHP\\Http\\'                     => dirname(__DIR__) . '/vendor/biurad/biurad-http/src',
+    'BiuradPHP\\FileManager\\'              => dirname(__DIR__) . '/vendor/biurad/biurad-flysystem/src',
+    'BiuradPHP\\DependencyInjection\\'      => dirname(__DIR__) . '/vendor/biurad/nette-di-bridge/src/',
+    'BiuradPHP\\'                           => dirname(__DIR__) . '/vendor/biurad/biurad-terminal/src',
+    'BiuradPHP\\'                           => dirname(__DIR__) . '/vendor/biurad/biurad-scaffolder/src',
+    'Cycle\\'                               => dirname(__DIR__) . '/vendor/divineniiquaye/cycle-orm/src',
+    'Cycle\\'                               => dirname(__DIR__) . '/vendor/divineniiquaye/cycle-annotation/src',
+    'Cycle\\'                               => dirname(__DIR__) . '/vendor/divineniiquaye/cycle-proxy-factory/src',
+    'Cycle\\'                               => dirname(__DIR__) . '/vendor/divineniiquaye/cycle-schema-builder/src',
+    'Flight\\Routing\\'  => dirname(__DIR__) . '/vendor/divineniiquaye/flight-routing/src',
+    'Prush\\'            => dirname(__DIR__) . '/vendor/divineniiquaye/prush-finder/src'
+];
+
+foreach ($autoloadProject as $namspace => $paths) {
+    $loader->setPsr4($namspace, $paths);
+}
+
+$loader->register(true);
+
+return $loader;
