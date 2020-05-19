7ca3675b0675321b91ee14331f1b305582842543
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-05-19 03:45:04 +0000

    Updated server configs

--- public/index.php
+++ public/index.php
@@ -3,19 +3,19 @@
 /*
  * This code is under BSD 3-Clause "New" or "Revised" License.
  *
- * ------------------------------------------------------------------------
- * BiuradPHP Framework is a new sheme of php artitecture which is simple,  |
- * yet has powerful features. The framework has been built carefully 	   |
- * following the rules of the new PHP 7.2 and 7.3 above, with no support   |
- * for the old versions of PHP. As this framework was inspired by          |
- * several conference talks about the future of PHP and its development,   |
- * this framework has the easiest and best approach to the PHP world,      |
- * of course, using a few intentinally procedural programming module.      |
- * This makes BiuradPHP framework extremely reable and usuable for all.    |
- * BiuradPHP is a 35% clone of symfony framwork and 30% clone of Nette	   |
- * framework. The perfomance of BiuradPHP is 600ms on development mode and |
- * on production mode it's even better with great defense security.        |                                            |
- * ------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * BiuradPHP Framework is a new scheme of php architecture which is simple,  |
+ * yet has powerful features. The framework has been built carefully 	     |
+ * following the rules of the new PHP 7.2 and 7.3 above, with no support     |
+ * for the old versions of PHP. As this framework was inspired by            |
+ * several conference talks about the future of PHP and its development,     |
+ * this framework has the easiest and best approach to the PHP world,        |
+ * of course, using a few intentionally procedural programming module.       |
+ * This makes BiuradPHP framework extremely readable and usable for all.     |
+ * BiuradPHP is a 35% clone of symfony framework and 30% clone of Nette	     |
+ * framework. The performance of BiuradPHP is 300ms on development mode and  |
+ * on production mode it's even better with great defense security.          |
+ * ---------------------------------------------------------------------------
  *
  * PHP version 7.2 and above required
  *
@@ -32,10 +32,10 @@ define('BR_START', microtime(true)) || defined('BR_START');
 
 /*
  * --------------------------------------------------------------------------
- * The Directory Seperator
+ * The Directory Separator
  * --------------------------------------------------------------------------
  *
- * This is to shorten the full write of directory seperator.
+ * This is to shorten the full write of directory separator.
  */
 define('DS', DIRECTORY_SEPARATOR) || defined('DS');
 
@@ -46,7 +46,24 @@ define('DS', DIRECTORY_SEPARATOR) || defined('DS');
  *
  * The full path starting from the index.php file. Improves performance (a bit)
  */
-define('BR_PATH', realpath(dirname(__FILE__, 2)) . DS) || defined('BR_PATH');
+define('BR_PATH', realpath(dirname(__FILE__, 2)).DS) || defined('BR_PATH');
+
+/*
+ *--------------------------------------------------------------------------
+ * Cli & CGI WebServer Booting
+ *-------------------------------------------------------------------------
+ *
+ * Decline static file requests back to the PHP built-in web-server
+ *
+ */
+if (in_array(PHP_SAPI, ['cli-server', 'cgi-fcgi'], true)) {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+
+    unset($path);
+}
 
 /**
  * --------------------------------------------------------------------------
@@ -57,23 +74,4 @@ define('BR_PATH', realpath(dirname(__FILE__, 2)) . DS) || defined('BR_PATH');
  * the calling script so we can separate the building of the instances
  * from the actual running of the application and sending responses.
  */
-$app = require BR_PATH . 'app' . DS . 'bootstrap.php';
-
-/*
- *--------------------------------------------------------------------------
- * Cli & CGI WebServer Booting
- *-------------------------------------------------------------------------
- *
- * Decline static file requests back to the PHP built-in webserver
- *
- */
-if (in_array(php_sapi_name(), ['cli-server', 'cgi-fcgi'], true)) {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (__FILE__ !== $path && is_file($path)) {
-        return false;
-    }
-
-    unset($path);
-}
-
-return $app; // Return The Application.
+require dirname(__DIR__).'/app/bootstrap.php';
